b9f25bbb94e1971ba242ec21ed55992bc68824a5
Jack Henza <jackhenza@gmail.com>
2023-11-28 11:48:54 -0500

    cli the orm

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,37 +1,98 @@
 from models.department import Department
 from models.employee import Employee
+from time import sleep
+from sys import stdout
 
 
 def exit_program():
-    print("Goodbye!")
+    scan_print("Goodbye!",0.1)
     exit()
 
 # We'll implement the department functions in this lesson
 
+def scan_print(s,t=0.01):
+    for c in s:
+        stdout.write(c)
+        stdout.flush()
+        sleep(t)
+    print()
 
 def list_departments():
-    pass
+    departments = Department.get_all()
+    for department in departments:
+        scan_print(f'{department}')
+    print()
+    input("Press Enter or Return to return to menu")
 
 
 def find_department_by_name():
-    pass
+    name = input("Enter the department's name: ")
+    department = Department.find_by_name(name)
+    if department:
+        scan_print(f'{department}')
+    else:
+        scan_print("...",0.5)
+        print(f'Department {name} not found')
+    input("Press Enter or Return to return to menu")
 
 
 def find_department_by_id():
-    pass
+    id_ = input("Enter the department's id: ")
+    department = Department.find_by_id(id_)
+    if department:
+        scan_print(f'{department}')
+    else:
+        scan_print("...",0.5)
+        print(f'Department {id_} not found')
+    input("Press Enter or Return to return to menu")
 
 
 def create_department():
-    pass
+    name = input("Enter the new department's name: ")
+    location = input("Enter the new department's location: ")
+    try:
+        department = Department.create(name, location)
+        scan_print(f'Department successfully created: {department}')
+    except Exception as ex:
+        scan_print(f"Error creating department: {ex}")
+    input("Press Enter or Return to return to menu")
 
 
 def update_department():
-    pass
+    id_ = input("Enter the id of the department to be updated: ")
+    if department := Department.find_by_id(id_):
+        scan_print(f'{department}')
+        try:
+            resp = input('Change name of department? y/n ')
+            if 'y' in resp:
+                name = input("Enter the department's new name: ")
+                department.name = name
+            resp = input('Change location of department? y/n ')
+            if 'y' in resp:
+                location = input("Enter the department's new location: ")
+                department.location = location
+            department.update()
+            scan_print(f'Success: {department}')
+        except Exception as ex:
+            scan_print(f'Error updating department: {ex}')
+    else:
+        scan_print('...',0.5)
+        print(f'Department {id_} not found')
+    print()
+    input("Press Enter or Return to return to menu")
 
 
 def delete_department():
-    pass
-
+    scan_print("Delete Department")
+    id_ = input("Enter the department's id: ")
+    if department := Department.find_by_id(id_):
+        department.delete()
+        scan_print(f'Department {id_} deleted')
+    else:
+        scan_print('...',0.5)
+        print(f'Department {id_} not found')
+    print()
+    input("Press Enter or Return to return to menu")
 
 # You'll implement the employee functions in the lab
 
